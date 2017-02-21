bd3667c8e922256606ff03c18b54d740a558092e
Axel-Girard <axel942@gmail.com>
2017-02-21 22:19:16 +0100

    only luck!

--- test.py
+++ test.py
@@ -3,20 +3,23 @@ from random import randint
 goal = 300
 
 def firstGen():
-    for i in range(10):
+    for i in range(100):
         a = randint(0,100)
         b = randint(0,100)
         c = randint(0,100)
         d = randint(0,100)
         e = randint(0,100)
         f = randint(0,100)
-        print("a+b+c+d+e+f: ", a+b+c+d+e+f)
-        rating(a,b,c,d,e,f)
+        if rating(a,b,c,d,e,f):
+            break
 
 def rating(a,b,c,d,e,f):
     res = a+b+c+d+e+f
-    if res == goal
-        print("gg, you've did it!")
+    if res == goal:
+        print(a, "+", b, "+", c, "+", d, "+", e, "+", f)
+        print("GG, you've did it! 300")
+        return True
+    return False
 
 
 firstGen()
